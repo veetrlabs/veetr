edec38972462c75f7a3e5cbd2884725cc406240e
Jan Linhart <admin@escope.cz>
2026-05-11 18:40:18 +0200

    Better responsiveness

--- app/src/components/MapButton.tsx
+++ app/src/components/MapButton.tsx
@@ -23,7 +23,7 @@ export default function MapButton() {
         viewBox="0 0 24 24" 
         fill="none" 
         stroke="currentColor" 
-        strokeWidth="2" 
+        strokeWidth="2.4" 
         strokeLinecap="round" 
         strokeLinejoin="round"
       >
